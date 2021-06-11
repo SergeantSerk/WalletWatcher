9e760f649ee936f9854c7e2a57aaa22b1a46dab3
SergeantSerk <14278530+SergeantSerk@users.noreply.github.com>
2021-06-11 22:46:31 +0100

    Dynamic data load + Nano implementation
    Added Datatables to dynamically load data into table instead of synchronously loading data and displaying all at once.
    Implemented Nano account explorer (third party).

--- scripts/main.js
+++ scripts/main.js
@@ -12,89 +12,82 @@ const ethWeb3 = new Web3(ethWeb3Provider);
 // Helium
 const hntEndpoint = 'https://api.helium.io/v1/accounts'
 
+// Nano
+const nanoEndpoint = 'https://api.nanocrawler.cc/v2/accounts'
+
 // Ravencoin
 const rvnEndpoint = 'https://explorer-api.ravenland.org/address';
 
 // Stellar
 const xlmServer = new StellarSdk.Server('https://horizon.stellar.org');
 
-async function loadTableData() {
-    var data = [];
+function loadTableData() {
+    wallets.forEach(processWallet);
+}
 
-    for (i = 0; i < wallets.length; ++i) {
-        var wallet = wallets[i];
-        switch (wallet.coin) {
-            case "DBIX":
-                await fetch(`${dbixEndpoint}/${wallet.address}`)
-                    .then(response => response.json())
-                    .then(json => json.info.find(item => item.hash.toLowerCase() === wallet.address.toLowerCase()))
-                    .then(result => makeRow(wallet, result.balance))
-                    .then(row => data.push(row))
-                    .catch(error => console.log(error));
-                break;
-            case "ETH":
-                await ethWeb3.eth.getBalance(wallet.address)
-                    .then(wei => convertWeiToEther(wei))
-                    .then(ether => makeRow(wallet, ether))
-                    .then(row => data.push(row))
-                    .catch(error => console.log(error));
-                break;
-            case "RVN":
-                await fetch(`${rvnEndpoint}/${wallet.address}/balances`)
-                    .then(response => response.json())
-                    .then(json => makeRow(wallet, json.data.RVN))
-                    .then(row => data.push(row))
-                    .catch(error => console.log(error));
-                break;
-            case "XCH":
-                await fetch(`${xchEndpoint}/${wallet.address}`)
-                    .then(response => response.json())
-                    .then(json => makeRow(wallet, convertPicoToChia(json.netBalance)))
-                    .then(row => data.push(row))
-                    .catch(error => console.log(error));
-                break;
-            case "XLM":
-                await xlmServer.loadAccount(wallet.address)
-                    .then(account => makeRow(wallet, account.balances[0].balance))
-                    .then(row => data.push(row))
-                    .catch(error => console.log(error));
-                break;
-            case "HNT":
-                await fetch(`${hntEndpoint}/${wallet.address}`)
-                    .then(response => response.json())
-                    .then(json => convertValueToHelium(json.data.balance))
-                    .then(balance => makeRow(wallet, balance))
-                    .then(row => data.push(row))
-                    .catch(error => console.log(error));
-                break;
-            default:
-                data.push(makeRow(wallet, null));
-                break;
-        }
+function processWallet(wallet, index) {
+    let table = $('#walletTable').DataTable();
+    switch (wallet.coin) {
+        case "DBIX":
+            fetch(`${dbixEndpoint}/${wallet.address}`)
+                .then(response => response.json())
+                .then(json => json.info.find(item => item.hash.toLowerCase() === wallet.address.toLowerCase()))
+                .then(account => table.row.add([wallet.coin, wallet.address, account.balance, wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        case "ETH":
+            ethWeb3.eth.getBalance(wallet.address)
+                .then(wei => convertDenominationToValue(1e18, wei))
+                .then(balance => table.row.add([wallet.coin, wallet.address, balance.toFixed(18), wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        case "RVN":
+            fetch(`${rvnEndpoint}/${wallet.address}/balances`)
+                .then(response => response.json())
+                .then(json => json.data.RVN)
+                .then(balance => table.row.add([wallet.coin, wallet.address, balance, wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        case "XCH":
+            fetch(`${xchEndpoint}/${wallet.address}`)
+                .then(response => response.json())
+                .then(json => convertDenominationToValue(1e12, json.netBalance))
+                .then(balance => table.row.add([wallet.coin, wallet.address, balance.toFixed(12), wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        case "XLM":
+            xlmServer.loadAccount(wallet.address)
+                .then(account => account.balances[0].balance)
+                .then(balance => table.row.add([wallet.coin, wallet.address, balance, wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        case "HNT":
+            fetch(`${hntEndpoint}/${wallet.address}`)
+                .then(response => response.json())
+                .then(json => convertDenominationToValue(1e8, json.data.balance))
+                .then(balance => table.row.add([wallet.coin, wallet.address, balance.toFixed(8), wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        case "NANO":
+            fetch(`${nanoEndpoint}/${wallet.address}`)
+                .then(response => response.json())
+                .then(json => convertDenominationToValue(1e30, json.account.balance))
+                .then(balance => table.row.add([wallet.coin, wallet.address, balance.toFixed(30), wallet.note]))
+                .then(render => render.draw(false))
+                .catch(error => console.log(error));
+            break;
+        default:
+            table.row.add([wallet.coin, wallet.address, "-", wallet.note]).draw(false);
+            break;
     }
-
-    $('#walletTable').bootstrapTable({ data: data })
-    document.getElementById('walletTable-spinner').style.display = "none";
 }
 
-function makeRow(wallet, balance) {
-    var row = {
-        coin: wallet.coin,
-        address: wallet.address,
-        balance: parseFloat(balance).toFixed(18),
-        note: wallet.note
-    };
-    return row;
-}
-
-function convertValueToHelium(helium) {
-    return parseFloat(helium) / 1e8;
-}
-
-function convertPicoToChia(chia) {
-    return parseFloat(chia) / 1e12;
-}
-
-function convertWeiToEther(ether) {
-    return parseFloat(ether) / 1e18;
+function convertDenominationToValue(denomination, value) {
+    return Number.parseFloat(value) / denomination;
 }
